9cb113234f24c0e6938bbd434d97bb63d3b5c063
m4klo <ul0258026@edu.uni.lodz.pl>
2022-04-11 11:42:15 +0200

    HTTP update

--- app/src/main/java/com/example/tinder_movie/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/example/tinder_movie/ChooseLoginRegistrationActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/tinder_movie/MainActivity.java
+++ app/src/main/java/com/example/tinder_movie/MainActivity.java
@@ -6,7 +6,12 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.example.tinder_movie.R;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.*;
+
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
@@ -17,13 +22,57 @@ public class MainActivity extends Activity {
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
+    private static HttpURLConnection connection;
 
+    public static void http() throws IOException {
+
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
+
+        String url = "https://unogs-unogs-v1.p.rapidapi.com/search/titles?offset=50&type=movie&limit=1";
+
+        URL obj = new URL(url);
+        connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        connection.setRequestProperty("X-RapidAPI-Host", "unogs-unogs-v1.p.rapidapi.com");
+        connection.setRequestProperty("X-RapidAPI-Key", "ae7b155a09msh17af395a573014dp1dc75bjsn7cb2bcd9773b");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        while((line=reader.readLine())!= null) {
+            responseContent.append(line);
+        }
+        reader.close();
+
+        System.out.println(responseContent.toString());
+    }
+
+  //  public static String parse(String responseBody) {
+  //      JSONArray albums = new JSONArray(responseBody);
+   //     for(int i=0; i<albums.length(); i++)
+   //     {
+  //          JSONObject album =albums.getJSONObject(i);
+   //         String title= album.getString("title");
+   //         System.out.println(title);
+   //     }
+
+  //  }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            http();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         al = new ArrayList<>();
         al.add("Apocalypse Now");
