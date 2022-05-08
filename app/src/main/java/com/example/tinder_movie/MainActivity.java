c4a56401a9a6d13318354f3058d9c7de3d668f63
maciej <ul0255085@edu.uni.lodz.pl>
2022-05-08 10:08:07 +0200

    ChooseRegistration
    Button i przejscie

--- app/src/main/java/com/example/tinder_movie/MainActivity.java
+++ app/src/main/java/com/example/tinder_movie/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tinder_movie;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -24,14 +25,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class MainActivity extends Activity {
 
-    String API_URL  = "https://unogs-unogs-v1.p.rapidapi.com/search/titles?offset=50&type=movie&limit=50";
+    String API_URL  = "https://unogs-unogs-v1.p.rapidapi.com/search/titles?offset=50&type=movie&limit=100";
     ProgressDialog progressDialog;
     private arrayAdapter arrayAdapter;
 
@@ -47,11 +46,15 @@ public class MainActivity extends Activity {
     public MainActivity() {
     }
 
-    public void loguoutUser(View view) {
+    public void logoutUser(View view) {
         mAuth.signOut();
         Intent intent = new Intent (MainActivity.this, ChooseLoginRegistrationActivity.class);
         startActivity(intent);
         finish();
+        return;
+    }
+    public void openLibrary(View view) {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
         return;
     }
 
@@ -137,8 +140,7 @@ public class MainActivity extends Activity {
         rowitems = new ArrayList<movies>();
         new syncData().execute();
         movies movie=new movies();
-        movie.setTitle("Swipe left or right!");
-        movie.setNetflixId("0");
+        movie.setTitle("Swipe left or right");
         rowitems.add(movie);
         arrayAdapter = new arrayAdapter(this, R.layout.item, rowitems);
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
@@ -148,26 +150,19 @@ public class MainActivity extends Activity {
             @Override
             public void removeFirstObjectInAdapter() {
                 Log.d("LIST", "removed object!");
-                String netflixid = rowitems.get(0).getNetflixId();
-                String title = rowitems.get(0).getTitle();
-                DatabaseReference currentUserDb = FirebaseDatabase.getInstance("https://movie-tinder-f5289-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Users").child(CurrentUId).child("right").child(netflixid);
-                Map movieInfo = new HashMap<>();
-                movieInfo.put("title", title);
-                movieInfo.put("netflixid", netflixid);
-                currentUserDb.updateChildren(movieInfo);
                 rowitems.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onLeftCardExit(Object dataObject) {
+
                 Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
                 Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
