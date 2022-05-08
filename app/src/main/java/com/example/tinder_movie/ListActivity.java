4f8218f8905197421906323848465e097b9de45d
m4klo <ul0258026@edu.uni.lodz.pl>
2022-05-08 11:30:32 +0200

    dodanie listy do obejrzenia

---
+++ app/src/main/java/com/example/tinder_movie/ListActivity.java
@@ -0,0 +1,82 @@
+package com.example.tinder_movie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ListActivity extends Activity {
+
+
+    private FirebaseAuth mAuth;
+    private String CurrentUId;
+
+    ListView listView;
+    ArrayList<String> myArrayList = new ArrayList<>();
+    DatabaseReference mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        listView = (ListView) findViewById(R.id.listview1);
+
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, myArrayList);
+
+        listView.setAdapter(myArrayAdapter);
+        mAuth = FirebaseAuth.getInstance();
+        CurrentUId = mAuth.getCurrentUser().getUid();
+
+        Log.d("uid", CurrentUId);
+
+        mRef = FirebaseDatabase.getInstance("https://movie-tinder-f5289-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Users").child(CurrentUId).child("right");
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Map<String,String> map = (Map) snapshot.getValue();
+                String title=map.get("title").toString();
+                myArrayList.add(title);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
